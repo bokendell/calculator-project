bfcea0d1ecb27e336c4d4847508ba3f360970e44
Aaron Kendell <aaronkendell@gmail.com>
2023-01-15 19:16:16 -0700

    Add js logic for operations and the switching the number sign

--- js-file.js
+++ js-file.js
@@ -1,3 +1,7 @@
+
+let previousNumber;
+let operationsClicked = 0;
+let currOperation;
 const display = document.querySelector('.display');
 const numbers = document.querySelectorAll('.number');
 numbers.forEach(number => number.addEventListener('click', function() {
@@ -10,11 +14,29 @@ function displayNumber(number) {
 
 const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', function() {
-    display.textContent = '';
+    clear();
 });
 
+const numSign = document.querySelector('.numSign');
+numSign.addEventListener('click', function() {
+    if (getCurrNumber().charAt(0) === '-'){
+        display.textContent = getCurrNumber().substring(1, getCurrNumber().length)
+    }
+    else{
+        display.textContent = '-' + getCurrNumber();
+    }
+})
+
+function getCurrNumber(){
+    return display.textContent;
+}
+
+function clear() {
+    display.textContent = '';
+}
+
 function add(num1, num2) {
-    return num1 + num2;
+    return parseInt(num1) + parseInt(num2);
 }
 
 function subtract(num1, num2) {
@@ -28,4 +50,37 @@ function divide(num1, num2) {
 function multiply(num1, num2) {
     return num1 * num2;
 }
+
+function operate(num1, num2, operation){
+    let result;
+    if (operation === '&divide;') {
+        result = divide(num1, num2);
+    }
+    if (operation === '&times;') {
+        result = multiply(num1, num2);
+    }
+    if (operation === '&minus;') {
+        result = subtract(num1, num2);
+    }
+    if (operation === '&plus;') {
+        result = add(num1, num2);
+    }
+    
+    previousNumber = result;
+
+}
+
+const operations = document.querySelectorAll('.operation');
+operations.forEach(operation => operation.addEventListener('click', function() {
+    operationsClicked++;
+    if (operationsClicked > 1) {
+        operate(previousNumber, getCurrNumber(), currOperation);
+    }
+    else {
+        previousNumber = getCurrNumber();
+        clear();
+    }
+    currOperation = operation.textContent;
+    operationsClicked = 1;
+}));
 
