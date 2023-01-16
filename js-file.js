4f8ee3f8375fa594a416846370e18d3b7ecea83e
Aaron Kendell <aaronkendell@gmail.com>
2023-01-15 20:25:24 -0700

    Change from int to float number

--- js-file.js
+++ js-file.js
@@ -56,22 +56,23 @@ function getCurrNumber(){
 
 function clear() {
     display.textContent = '';
+    containsDecimal = false;
 }
 
 function add(num1, num2) {
-    return parseInt(num1) + parseInt(num2);
+    return parseFloat(num1) + parseFloat(num2);
 }
 
 function subtract(num1, num2) {
-    return parseInt(num1) - parseInt(num2);
+    return parseFloat(num1) - parseFloat(num2);
 }
 
 function divide(num1, num2) {
-    return parseInt(num1) / parseInt(num2);
+    return parseFloat(num1) / parseFloat(num2);
 }
 
 function multiply(num1, num2) {
-    return parseInt(num1) * parseInt(num2);
+    return parseFloat(num1) * parseFloat(num2);
 }
 
 function operate(num1, num2, operation){
